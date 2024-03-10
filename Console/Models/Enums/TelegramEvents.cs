f8fd7964c67b70a966fba633144e70b28384a2f6
Владимир Ушаков <svayznoy@inbox.ru>
2024-03-10 23:42:25 +0700

    Написать перечислитель TelegramEvents

---
+++ Console/Models/Enums/TelegramEvents.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace Console.Models.Enums
+{
+	public enum TelegramEvents
+	{
+		[Description(nameof(Initialization))]
+		Initialization,
+
+		[Description(nameof(Register))]
+		Register,
+
+		[Description(nameof(Message))]
+		Message,
+
+		[Description(nameof(Server))]
+		Server,
+
+		[Description(nameof(BlockedBot))]
+		BlockedBot,
+
+		[Description(nameof(CommandExecute))]
+		CommandExecute,
+
+		[Description(nameof(GroupAction))]
+		GroupAction,
+	}
+}

e99d4a58fe652fc04f095be996aa86780f861808
Владимир Ушаков <svayznoy@inbox.ru>
2024-03-10 23:09:19 +0700

    Создать экземпляр Телеграм-бота

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -3,9 +3,38 @@
 
 
 
+#region [запуск telegram бота]
+using PRTelegramBot.Core;
 
-//Ожидание ввода команды
+var telegram = new PRBot(options =>
+{
+	// Токен telegram бота берется из BotFather
+	options.Token = "6605743715:AAHjxHXOVDCA0dB0ruWWzFwzW1fBr7uUEyY";
+	// Перед запуском очищает список обновлений, которые накопились когда бот не работал.
+	options.ClearUpdatesOnStart = true;
+	// Если есть хоть 1 идентификатор telegram пользователя, могут пользоваться только эти пользователи
+	options.WhiteListUsers = new List<long>() { };
+	// Идентификатор telegram пользователя
+	options.Admins = new List<long>() { };
+	// Уникальных идентификатор для бота, используется, чтобы в одном приложение запускать несколько ботов
+	options.BotId = 0;
+});
+
+await telegram.Start();
+
+#endregion [запуск telegram бота]
+
+
+#region [Держать консоль открытой]
+
+//Команда для завершения приложения
 const string EXIT_COMMAND = "exit";
+//Запуск программы
+Console.WriteLine("Запуск программы");
+Console.WriteLine($"Для закрытия программы напишите {EXIT_COMMAND}");
+//Ожидание ввода команды
 while(true)
 	if(Console.ReadLine().ToLower() == EXIT_COMMAND)
 		Environment.Exit(0);
+
+#endregion [Держать консоль открытой]

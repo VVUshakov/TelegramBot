4681683c0826a2461167c01332c1918268cc4896
Владимир Ушаков <svayznoy@inbox.ru>
2024-03-13 22:11:42 +0700

    Создать модель "Статус/привелегии пользователя"

---
+++ TelegramBot/Models/UserPrivilege.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace ConsoleApp.Models.Enums
+{
+	/// <summary>
+	/// Статус/Привилегии пользователей
+	/// </summary>
+	[Flags]
+	public enum UserPrivilege
+	{
+		[Description("Гость")]
+		Guest = 1,
+
+		[Description("Зарегистрированный")]
+		Registered = 2,
+
+		[Description("Администратор")]
+		Admin = 4,
+
+		[Description("VIP")]
+		VIP = 8,
+
+		[Description("Модератор")]
+		Moderator = 16,
+	}
+}
